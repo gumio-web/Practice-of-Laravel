1a9575d4cf26a49a6580a8a4d3a5fb5bf42a6f18
gumio-web <84694177+gumio-web@users.noreply.github.com>
2021-07-10 18:14:58 +0900

    Eloquent\Model

---
+++ app/Person.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
+
+class Person extends Model
+{
+    protected $guarded = ['id',];
+    public static $rules = [
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    ];
+
+    public function getData()
+    {
+        return "{$this->id}: {$this->name}({$this->age})";
+    }
+}
--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.helloapp')
 
-@section('title', 'Index')
+@section('title', 'Person.index')
 
 @section('menubar')
 @parent
@@ -10,15 +10,11 @@
 @section('content')
 <table>
     <tr>
-        <th>Name</th>
-        <th>Mail</th>
-        <th>Age</th>
+        <th>Data</th>
     </tr>
-    @foreach($items as $item)
+    @foreach ($items as $item)
     <tr>
-        <td>{{$item->name}}</td>
-        <td>{{$item->mail}}</td>
-        <td>{{$item->age}}</td>
+        <td>{{$item->getData()}}</td>
     </tr>
     @endforeach
 </table>
